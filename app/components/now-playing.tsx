c423fde48b6d1af59827f03c92e5aabc2b03c2f5
thienjs <thien.tran6@gmail.com>
2025-10-06 12:44:57 -0400

    skeleton change

--- app/components/now-playing.tsx
+++ app/components/now-playing.tsx
@@ -214,12 +214,12 @@ export function NowPlaying() {
   // Loading state skeleton
   if (isLoading) {
     return (
-      <div className='w-full mx-2 mb-1'>
+      <div className=''>
         <div className='flex items-center space-x-4'>
-          <Skeleton className='h-12 w-12 rounded-lg flex-shrink-0' />
+          <Skeleton className='h-8 w-8 rounded-lg flex-shrink-0' />
           <div className='flex-1 space-y-2'>
-            <Skeleton className='h-4 w-32' />
-            <Skeleton className='h-3 w-24' />
+            <Skeleton className='h-4 w-16' />
+            <Skeleton className='h-3 w-8' />
           </div>
           <div className='flex space-x-2'>
             <Skeleton className='h-8 w-16' />
